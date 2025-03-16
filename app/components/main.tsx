5a0e3e987d0cb35b29c16e52f669c3f52f16d57c
JustGabo <ovallegabriel123@gmail.com>
2025-03-16 04:01:14 -0300

    running build

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -1,8 +1,6 @@
 "use client";
 import Lenis from "lenis";
-import React, { useEffect, useState } from "react";
-// import DivingGallery from "@/app/components/diving-gallery";
-// import HorizontalScroll from "./horizontal-scroll";
+import React, { useEffect} from "react";
 import Hero from "./hero";
 import Agenda from "./agenda";
 import Speakers from "./speakers";
@@ -12,8 +10,6 @@ import Data from "./data";
 import Footer from "./footer";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Details from "./details";
-import { useProjectContext } from "../context/projectContext";
 import Collaborations from "./collaborations";
 const MainPage = () => {
   gsap.registerPlugin(ScrollTrigger)
@@ -27,21 +23,6 @@ const MainPage = () => {
 
     requestAnimationFrame(raf);
   }, []);
-  const { project } = useProjectContext();
-  const [bottomBar, setBottomBar] = useState(false);
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    if (project) {
-      setIsMounted(true);
-    }
-
-    setBottomBar(true);
-    setTimeout(() => {
-      setBottomBar(false);
-    }, 700);
-  }, [project]);
-
   return (
     <div className="">
       <Hero />
@@ -49,25 +30,10 @@ const MainPage = () => {
       <Speakers />
 
       <Slider />
-      {/* <div className="relative overflow-hidden">
-        <div className={`${!project ? "opacity-100" : "opacity-0"}`}>
-          <Slider />
-        </div>
-        <div
-          style={{ transform: `translateY(${isMounted ? "0" : "-100dvh"})` }}
-          className={`absolute top-0 left-0 w-full z-50 transition-all ease-in-out duration-1000 ${
-            project ? "block" : "hidden"
-          }`}
-        >
-          <Details />
-        </div>
-      </div> */}
       <Schedule />
       <Data />
       <Collaborations />
       <Footer />
-      {/* <DivingGallery /> */}
-      {/* <HorizontalScroll/> */}
     </div>
   );
 };
